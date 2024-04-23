0bbca6d86e15b3a31506d87422727eb1ab4c755e
corentinventejol <corentinventejol78@gmail.com>
2024-04-23 15:11:58 +0200

    wip

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,31 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name); // Ajoutez l'attribut alt avec le nom du photographe
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+        const pLocation = document.createElement('p');
+        pLocation.textContent = `${city}, ${country}`; // Placer le pays à côté de la ville
+        const pTagline = document.createElement('p');
+        pTagline.textContent = `${tagline}`;
+        const pPrice = document.createElement('p');
+        pPrice.textContent = `${price} /Jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(pLocation);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    // Retourne les données nécessaires
+    return { name, picture, getUserCardDOM };
 }
