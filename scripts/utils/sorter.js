dc7fce97283d3fc457dc79db34af5c3b55b54e1b
corentinventejol <corentinventejol78@gmail.com>
2024-08-19 15:08:02 +0200

    ajout de commentaire

--- scripts/utils/sorter.js
+++ scripts/utils/sorter.js
@@ -1,4 +1,4 @@
-// La fonction sortMedia() trie un tableau de medias selon le critère spécifié.
+// La fonction sortMedia() trie un tableau de medias selon un critère spécifié. 
 // - a et b sont des parametres qui indique l'ordre de trie decroissant de b -> a et croissant de a -> b
 
 export function sortMedia(medias, criteria) {
@@ -7,7 +7,7 @@ export function sortMedia(medias, criteria) {
     } else if (criteria === 'date') {
         return medias.sort((a, b) => new Date(b.date) - new Date(a.date));
     } else if (criteria === 'titre') {
-        return medias.sort((a, b) => a.title.localeCompare(b.title));
+        return medias.sort((a, b) => a.title.localeCompare(b.title)); // méthode qui compare deux chaînes de caractères en utilisant les règles de comparaison de l'ordre alphabétique
     }
     return medias;
 }
